3c1d61d7118f6f2ed209d9328a5bc766a9588480
freedomAsWind <724651142@qq.com>
2016-01-13 16:08:02 +0800

    添加了导航控制器,以及tabbar控制器

--- LearningCircle/LearningCircle/main(主框架)/others/AppDelegate.m
+++ LearningCircle/LearningCircle/main(主框架)/others/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "LEOMainTabBarController.h"
 @interface AppDelegate ()
 
 @end
@@ -16,7 +16,12 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+
+    self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    LEOMainTabBarController *mainVc = [[LEOMainTabBarController alloc]init];
+    self.window.rootViewController = mainVc;
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
